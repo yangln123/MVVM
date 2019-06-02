c51937908c65ea0114697deeb08ca65117bfdb9f
yangln <yanglunan@qiyi.com>
2019-06-02 22:02:16 +0800

    net添加

--- Demo/MVVM_RACDemo/MVVM_RACDemo/Service/Tools/JRUtility.m
+++ Demo/MVVM_RACDemo/MVVM_RACDemo/Service/Tools/JRUtility.m
@@ -24,4 +24,38 @@
     return dict;
 }
 
++ (void)sendGetRequestWithUrl:(NSString *)url params:(NSDictionary *)params finish:(void(^)(id data, NSError *error))finish {
+    AFSecurityPolicy *securityPolicy = [AFSecurityPolicy defaultPolicy];
+    securityPolicy.validatesDomainName = NO;
+    securityPolicy.allowInvalidCertificates = YES;
+    
+    [AFHTTPSessionManager manager].securityPolicy = securityPolicy;
+    
+    [[AFHTTPSessionManager manager] GET:url parameters:params progress:^(NSProgress * _Nonnull downloadProgress) {
+        
+    } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        if (finish) {
+            finish(responseObject, nil);
+        }
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        if (finish) {
+            finish(nil, error);
+        }
+    }];
+}
+
++ (void)sendPostRequestWithUrl:(NSString *)url params:(NSDictionary *)params finish:(void(^)(id data, NSError *error))finish {
+    [[AFHTTPSessionManager manager] POST:url parameters:params progress:^(NSProgress * _Nonnull uploadProgress) {
+        
+    } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        if (finish) {
+            finish(responseObject, nil);
+        }
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        if (finish) {
+            finish(nil, error);
+        }
+    }];
+}
+
 @end
